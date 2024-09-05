1501b08652fefe8f08c09bc636eb698040295ab0
23DP2DTuma <166400530+23DP2DTuma@users.noreply.github.com>
2024-09-05 10:09:59 +0000

    Uzdevums 0

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,16 +1,25 @@
 package lv.rvt;
 
+
+
 import java.util.Scanner;
 
 public class App 
 {
-    public static void main( String[] args )
+
+    public static void main(String[] args) 
     {
-        
-
         Scanner scanner = new Scanner(System.in);
-        
 
+        System.out.println("Uzraksti savu vārdu: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Uzraksti savu Uzvārdu: ");
+        String username = scanner.nextLine();
+      
+        System.out.println("Uzraksti savu grupu: ");
+        String group = scanner.nextLine();
+
+        System.out.println(name + " " + username + " " + group + " ");
     }
-
 }
