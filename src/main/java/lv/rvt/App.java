04b353e61c5498ec4d56c41474fa2c848a8c26ca
23DP2DTuma <166400530+23DP2DTuma@users.noreply.github.com>
2024-09-12 09:34:34 +0000

    Simple calculator

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -13,37 +13,11 @@ public class App
         int first = Integer.valueOf(scanner.nextLine());
         System.out.println("Give the second number:");
         int second = Integer.valueOf(scanner.nextLine());
-
-
+        
         System.out.println(first + " + " + second + " = " + (first + second));
-
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        //int number = 23;
-
-        //int inputAsInt = Integer.valueOf(scanner.nextLine());
-
-        //double inputAsDouble = Integer.valueOf(scanner.nextLine());
-
-        //boolean inputAsBoool = Boolean.valueOf(scanner.nextLine());
-
-        //System.out.println(inputAsInt);
-        //System.out.println(inputAsDouble);
-        //System.out.println(inputAsBoool);
+        System.out.println(first + " - " + second + " = " + (first - second));
+        System.out.println(first + " * " + second + " = " + (first * second));
+        System.out.println(first + " / " + second + " = " + (double) (first / second));
 
     }
 }
