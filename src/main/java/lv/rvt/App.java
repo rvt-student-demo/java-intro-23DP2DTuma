b63c76558d9c15ef0766644c10f5eb96add18cb7
23DP2DTuma <166400530+23DP2DTuma@users.noreply.github.com>
2024-09-19 07:58:01 +0000

    FizzBuzz

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,25 +8,17 @@ public class App
     public static void main( String[] args ) {
     
     Scanner reader = new Scanner(System.in);
-    System.out.println("Grade points [0-100]:");
+    
     int number = Integer.valueOf(reader.nextLine());
-
-    if (number < 0) {
-        System.out.println("Grade: Impossible!");
-    } else if (number <= 49) {
-        System.out.println("Grade: failed");
-    } else if (number <= 59) {
-        System.out.println("Grade: 1");
-    } else if (number <= 69) {
-        System.out.println("Grade: 2");
-    } else if (number <= 79) {
-        System.out.println("Grade: 3");
-    } else if (number <= 89) {
-        System.out.println("Grade: 4");
-    } else if (number <= 100) {
-        System.out.println("Grade: 5");
+    
+    if (number % 3 == 0) {
+        System.out.println("Fizz");
+    } else if (number % 5 == 0) {
+        System.out.println("Buzz");
+    } else if (number % 3 == 0 && number % 5 == 0) {
+        System.out.println("FizzBuzz");
     } else {
-        System.out.println("Grade: Incresible!");
+        System.out.println(number);
     }
 
     
