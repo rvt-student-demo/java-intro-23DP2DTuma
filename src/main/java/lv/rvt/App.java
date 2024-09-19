68657f3e8027fc16dc90b01bea11b959760e13cb
23DP2DTuma <166400530+23DP2DTuma@users.noreply.github.com>
2024-09-19 10:31:42 +0000

    Only positives

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,28 +6,27 @@ public class App
 {
 
     public static void main( String[] args ) {
-    
-    Scanner reader = new Scanner(System.in);
-    System.out.println("Value of the gift?");
-    int Itemprice = Integer.valueOf(reader.nextLine());
-    
-    if (Itemprice >= 5000 && Itemprice < 25000) {
-        System.out.println("Tax: " + (100+ (Itemprice - 5000) * 0.08));
-    } else if (Itemprice < 5000) {
-        System.out.println("No Tax!");
-    } else if (Itemprice >= 25000 && Itemprice < 55000) {
-        System.out.println("Tax: " + (1700+ (Itemprice - 25000) * 0.10));
-    } else if (Itemprice >= 55000 && Itemprice < 200000) {
-        System.out.println("Tax: " + (4700+ (Itemprice - 55000) * 0.12));
-    } else if (Itemprice >= 200000 && Itemprice < 1000000) {
-        System.out.println("Tax: " + (22100+ (Itemprice - 200000) * 0.15));
-    } else if (Itemprice >= 1000000) {
-        System.out.println("Tax: " + (142100+ (Itemprice - 1000000) * 0.17));
+        
+        Scanner scanner = new Scanner(System.in);
+
+        
+        while (true) {
+            System.out.println("Give a number:");
+            int number = Integer.valueOf(scanner.nextLine());  
+            if (number == 0) {
+                break;
+            }
+        
+            if (number < 0) {
+                System.out.println("Unsuitable number");
+                continue;
+            }
+            System.out.println(number * number);
+        }
+        
     }
 
-
-
-
-}
+    
 
 }
+
