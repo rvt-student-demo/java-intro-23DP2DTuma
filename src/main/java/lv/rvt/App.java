b914de1b7ca687bb61127417ef04fd00e0f620b9
23DP2DTuma <166400530+23DP2DTuma@users.noreply.github.com>
2024-10-03 08:06:22 +0000

    Repeating, breaking and remembering (5 parts)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,24 +7,56 @@ public class App
 
     public static void main( String[] args ) {
         Scanner scanner = new Scanner(System.in);
-        int i = 1;
-        int fact = 1;
-        System.out.println("Give a number: ");
-        int number2 = Integer.valueOf(scanner.nextLine());
-        for (i = 2; i <= number2; i++) {
-            fact = fact * i;
+
+        int sum = 0;
+        int ones = 0;
+        int evenNumbers = 0;
+        int oddNumbers = 0;
+        System.out.println("Give a number:");
+        while (true) {
+            int number = Integer.valueOf(scanner.nextLine());  
+            
+            if (number == -1) {
+                System.out.println("Thx! Bye!");
+                System.out.println("Sum:" + sum);
+                System.out.println("Numbers: " + ones);
+                System.out.println("Average: " + (double)sum / ones);
+                System.out.println("Even: " + evenNumbers);
+                System.out.println("Odd: " + oddNumbers);
+                break;
+            }
+            
+            if (number % 2 == 0) {
+                evenNumbers++;
+                continue;
+            }
+
+            if (number % 2 != 0) {
+                oddNumbers++;
+                continue;
+            }
+
+            
+
+            else {
+                sum += number;
+                ones += 1;
+            
         }
-        System.out.println("Factorial: " + fact);
+        
+        
+
+
+
+
+
+
     }
 
-        
-        
-        
+
+
 }
-        
     
-
-    
-
+}
 
 
