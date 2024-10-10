ed335c85b4601de1a6ec9b408f6ae1b7e2057af5
23DP2DTuma <166400530+23DP2DTuma@users.noreply.github.com>
2024-10-10 08:52:41 +0000

    Star sign (4 parts)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,30 +1,15 @@
 package lv.rvt;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class App 
 {
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int number1 = 2;
-        int number2 = 10;
-        divisibleByThreeInRange(2,10);
-        
-    
-    }
-    public static void divisibleByThreeInRange(int beginning, int end){
-        while (beginning <= end){
-            if (beginning % 3 == 0){
-                System.out.println(beginning);
-                
-            }
-            beginning++;
-      
-        }
-    }
-
+public static void main(String[] args) {
+    Stars.printTriangle(4);
 }
+}
+
+
 
 
 
---
+++ src/main/java/lv/rvt/Stars.java
@@ -0,0 +1,43 @@
+package lv.rvt;
+
+import java.util.*;
+
+public class Stars {
+    public static void printStars(int number) {
+        for(int i = 0; i < number; i++) {
+            System.out.print("*");
+        }
+        
+    
+        }
+    public static void printSquare(int size) {
+        for(int i = 0; i < size; i++){
+            printStars(size);
+            System.out.print("\n");
+        }
+    }
+    public static void printRectangle(int width, int height) {
+        int number = 0;
+        while (number < width && number < height) {
+
+            printStars(width);
+            System.out.print("\n");
+            number++;
+
+        }
+
+    }
+    public static void printTriangle(int size) {
+        int number = 0;
+        while (number <= size) {
+
+            printStars(number);
+            System.out.print("\n");
+            number++;
+        }
+
+    }
+
+
+    
+}
