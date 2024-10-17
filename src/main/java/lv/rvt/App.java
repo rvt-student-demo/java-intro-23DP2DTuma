11629f792aca8370ada2e0b14a0bc4a2d40d28f6
23DP2DTuma <166400530+23DP2DTuma@users.noreply.github.com>
2024-10-17 08:07:30 +0000

    second plus third

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,11 +4,35 @@ import java.util.*;
 
 public class App 
 {
-public static void main(String[] args) {
-    Stars.printTriangle(4);
-}
-}
+    public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        
+        ArrayList<Integer> numbers = new ArrayList<>();
+        
+        while (true) {
+        
+        int number = Integer.valueOf(scanner.nextLine());
+        
+        if (number == 0) {
+        
+        break;
+        
+        }
+        
+        numbers.add(number);
+    }
+        int total_sum = 0;
+        for (int sum : numbers){
+        total_sum += sum;
+        }
+        
+        
+        System.out.println(numbers.get(1) + numbers.get(2));
+        System.out.println(total_sum);
+        }
+        
+        }
 
 
 
