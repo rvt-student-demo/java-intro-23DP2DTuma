8c380cc4a6237225aa91e68f8a014c68b2ca5d01
23DP2DTuma <166400530+23DP2DTuma@users.noreply.github.com>
2024-10-24 10:30:06 +0000

    Product

---
+++ src/main/java/lv/rvt/main.java
@@ -0,0 +1,17 @@
+package lv.rvt;
+
+import java.util.*;
+
+
+public class main {
+    public static void main(String[] args) {
+        Product sdf = new Product("Banana",1.1,13);
+        sdf.printProduct();
+    }
+}
+
+    
+
+
+
+
