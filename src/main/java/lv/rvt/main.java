97c4af5463434771f24d19b47b4615224e22314a
23DP2DTuma <166400530+23DP2DTuma@users.noreply.github.com>
2024-10-30 11:31:03 +0000

    Decreasing counter (3 parts)

--- src/main/java/lv/rvt/main.java
+++ src/main/java/lv/rvt/main.java
@@ -5,10 +5,18 @@ import java.util.*;
 
 public class main {
     public static void main(String[] args) {
-        Product sdf = new Product("Banana",1.1,13);
-        sdf.printProduct();
+        DecreasingCounter counter = new DecreasingCounter(100);
+
+        counter.printValue();
+
+        counter.reset();
+        counter.printValue();
+
+        
+        counter.printValue();
     }
 }
+
 
     
 
