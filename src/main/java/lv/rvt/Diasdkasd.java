681a559214eda9ea9aedb7908e979ded9270aacd
23DP2DTuma <166400530+23DP2DTuma@users.noreply.github.com>
2024-11-06 11:28:35 +0000

    Agent

---
+++ src/main/java/lv/rvt/Diasdkasd.java
@@ -0,0 +1,14 @@
+package lv.rvt;
+
+public class Agent {
+    public class Pe
+    
+        
+         
+    
+        public boolean isOfLegalAge() {
+            return this.age >= 18;
+        }
+        */
+    }
+}
--- src/main/java/lv/rvt/main.java
+++ src/main/java/lv/rvt/main.java
@@ -5,20 +5,18 @@ import java.util.*;
 
 public class main {
     public static void main(String[] args) {
-        DecreasingCounter counter = new DecreasingCounter(100);
-
-        counter.printValue();
-
-        counter.reset();
-        counter.printValue();
-
-        
-        counter.printValue();
-    }
-}
+        Agent bond = new Agent("James", "Bond");
+        bond.toString(); // prints nothing
+        System.out.println(bond.toString());
+        Agent ionic = new Agent("Ionic", "Bond");
+        System.out.println(ionic.toString());
 
 
     
+}
+
+
+}
 
 
 
