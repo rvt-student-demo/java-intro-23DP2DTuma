8a6f3931ed74b355136ad56f61bc79a9dc20f842
23DP2DTuma <166400530+23DP2DTuma@users.noreply.github.com>
2024-11-28 11:10:01 +0000

    Health station

---
+++ src/main/java/lv/rvt/HealthStation.java
@@ -0,0 +1,26 @@
+package lv.rvt;
+import java.util.*;
+import java.io.*;
+import java.nio.file.StandardOpenOption;
+
+public class HealthStation {
+    
+    private int weighingCount = 0;
+
+
+    public int weigh(Person person) {
+        weighingCount += 1;
+        return person.getWeight();
+    }
+    
+    public void feed(Person person){
+        
+        person.setWeight(person.getWeight() + 1);
+    }
+    public int weighings(){
+       
+        
+        return weighingCount;
+    }
+
+}
--- src/main/java/lv/rvt/Person.java
+++ src/main/java/lv/rvt/Person.java
@@ -1,23 +1,67 @@
 package lv.rvt;
+import java.util.*;
 import java.io.*;
-import java.util.ArrayList;
+import java.nio.file.StandardOpenOption;
+
 
 public class Person {
-    String name;
-    int age;
-    double weight;
-    double height;
 
-    // Constructor with 4 parameters
-    public Person(String name, int age, double weight, double height) {
+    private String name;
+    private int age;
+    private int height;
+    private int weight;
+
+    public Person(String name, int age, int height, int weight) {
         this.name = name;
         this.age = age;
-        this.weight = weight;
         this.height = height;
+        this.weight = weight;
+    }
+
+    public void printPerson() {
+        System.out.println("My name is " + this.name + " and I am " + this.age + " years old");
+    }
+
+    public void growOlder() {
+        this.age++;
+    }
+
+    public boolean isOfLegalAge() {
+        if (this.age > 17) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getHeight() {
+        return this.height;
+    }
+
+    public int getWeight() {
+        return this.weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public double bmi() {
+        double heightInMeters = this.height / 100.0;
+
+        return this.weight / (heightInMeters * heightInMeters);
+    }
+
+    public String getName() {
+        return this.name;
     }
 
     @Override
     public String toString() {
-        return "Name: " + name + ", Age: " + age + ", Weight: " + weight + ", Height: " + height;
+        return "My name is " + this.name + " and I am " + this.age + " years old. My BMI is " + this.bmi();
     }
 }
